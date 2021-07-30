192cd96a112776ccd2ee7ae4cc56bbe5f19314ac
devar <devar@JinkasComps>
2021-07-30 21:15:16 +0530

    new project - first commit

---
+++ Automation/src/General/Sample.java
@@ -0,0 +1,16 @@
+package General;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+	}
+
+}
